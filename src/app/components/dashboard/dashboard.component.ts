75b96b50ed60bc61ba8ffec806fd81fd31bc6ca3
relibrana <becabn24@gmail.com>
2021-05-21 23:58:53 -0500

    app.components

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import User from 'src/app/models/user.model';
+import { StorageService} from './../../services/storage.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  user: User
+  constructor(private storageService: StorageService, private router: Router) { }
 
   ngOnInit(): void {
+    this.user = this.storageService.getCurrentUser();
+    console.log(this.user);
+  }
+  logout(){
+    this.storageService.logout();
+  }
+  openSavingPlan() {
+    this.router.navigate(['/saving-plan']);
   }
 
 }
