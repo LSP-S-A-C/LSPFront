0cb959f6344c2b0d76fa977901afed115b1a6da5
ElenaSalcedo <elenadanielass@hotmail.com>
2021-06-05 11:53:36 -0500

    Update Routing Modules for Mis Datos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { SavingPlanComponent } from './components/saving-plan/saving-plan.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { MisDatosComponent } from './components/mis-datos/mis-datos.component';
 import { AuthorizationGuard } from './guards/AuthorizationGuard';
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent},
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthorizationGuard]},
-  { path: 'saving-plan', component: SavingPlanComponent, canActivate: [AuthorizationGuard]}
+  { path: 'saving-plan', component: SavingPlanComponent, canActivate: [AuthorizationGuard]},
+  { path: 'mis-datos', component: MisDatosComponent, canActivate: [AuthorizationGuard]}
 ];
 
 @NgModule({
