5b52db7d24f6a1f5d35b97614292a86b3545d493
ElenaSalcedo <elenadanielass@hotmail.com>
2021-06-10 22:33:44 -0500

    Update environment access link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  backend_url: "https://lsp-backend.herokuapp.com/api/v1/"
-
+  backend_url: "http://lsp-user-api.herokuapp.com/api/v1/swagger-ui.html#/"
 };
