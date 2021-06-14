39193ecd1f41dc02c2696aa2ca9f675d15f34297
ElenaSalcedo <elenadanielass@hotmail.com>
2021-06-14 00:10:59 -0500

    For Functional Front

--- src/app/services/saving-sheets.service.ts
+++ src/app/services/saving-sheets.service.ts
@@ -2,19 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SavingSheetsContainer, SavingSheets, Category, CashFlow} from "../models/finanzas";
-import {SessionContainer  }from "../models/session.model"
+import {SessionContainer  }from "../models/session.model"/*
 @Injectable({
   providedIn: 'root'
 })
-export class SavingSheetsService {
+/*export class SavingSheetsService {
   apiEndPoint: string="";
   constructor(private http: HttpClient) {    
   }
    findAll(): Observable<SavingSheetsContainer> {
      return this.http.get<SavingSheetsContainer>("https://lsp-ahorros-api.herokuapp.com/api/v1/savingplan")
    }
-  save(currency: string, currentMoney: number, currentSaves: number, savesPercent: number, userId: number, savesgoals: Info[]):  Observable<SavingPlanContainer> {
+  savesheet(currency: string, currentMoney: number, currentSaves: number, savesPercent: number, userId: number, savesgoals: Info[]):  Observable<SavingPlanContainer> {
     return this.http.post<SavingSheetsContainer>("https://lsp-ahorros-api.herokuapp.com/api/v1/savingplan",{currency: currency, currentMoney: currentMoney,
      currentSaves: currentSaves, savesPercent: savesPercent, userId: userId, savesgoals: savesgoals})
   }
-}
+}*/
